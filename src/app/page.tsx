bec8e1e6395456bd5c9dae560533125ac5c75fcb
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2025-01-14 16:30:43 +0900

    build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { SearchForm } from "@/app/components/search-form";
 import { RepositoryListWrapper } from "@/app/components/repository-list-wrapper";
 import { Orbs } from "@/app/components/orbs";
@@ -9,8 +10,14 @@ export default function Home() {
       <h1 className="text-4xl font-bold text-center mb-8 text-primary">
         GitHubリポジトリ検索
       </h1>
-      <SearchForm />
-      <RepositoryListWrapper />
+      <Suspense>
+        <SearchForm />
+      </Suspense>
+      <Suspense
+        fallback={<div className="text-center mt-8">読み込み中...</div>}
+      >
+        <RepositoryListWrapper />
+      </Suspense>
     </main>
   );
 }
